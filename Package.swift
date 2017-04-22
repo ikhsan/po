32a421e011a5b90d263b577bb069b329e5d5b103
Ikhsan Assaat <ikhsan.assaat@songkick.com>
2017-04-22 23:21:03 +0100

    Update stencil

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 8),
+        .Package(url: "https://github.com/ikhsan/Kitura-StencilTemplateEngine.git", majorVersion: 1, minor: 9),
     ]
 )
