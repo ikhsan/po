715e352591823d96ed3a9adb693d5b20c963ec6b
Ikhsan Assaat <ixnixnixn@yahoo.com>
2017-04-30 01:23:03 +0100

    Add simple caching in sheets

--- Sources/poCore/Sheets.swift
+++ Sources/poCore/Sheets.swift
@@ -9,10 +9,43 @@ public struct SheetsError: Error {
     }
 }
 
+class SheetsCache {
+
+    private struct TimedJSON {
+        let json: JSON
+        let timestamp: Date
+    }
+
+    private let timeout: TimeInterval
+    private var storage: [String : TimedJSON] = [:]
+
+    init(timeout: TimeInterval = 60.0) {
+        self.timeout = timeout
+    }
+
+    subscript(index: String) -> JSON? {
+        get {
+            guard let value = storage[index] else { return nil }
+            if abs(value.timestamp.timeIntervalSinceNow) > timeout {
+                storage.removeValue(forKey: index)
+                return nil
+            }
+            
+            return value.json
+        }
+        set(newJSON) {
+            guard let json = newJSON else { storage.removeValue(forKey: index); return; }
+            storage[index] = TimedJSON(json: json, timestamp: Date())
+        }
+    }
+
+}
+
 public struct Sheets {
 
     private let session = URLSession(configuration: URLSessionConfiguration.default)
     private let baseUrl = URL(string: "https://sheets.googleapis.com/v4/spreadsheets/")!
+    private var cache = SheetsCache()
 
     let apiKey: String
 
@@ -27,14 +60,21 @@ public struct Sheets {
 
     public func getValue(forSheetId sheetId: String, name: String) throws -> JSON {
         let path = "\(sheetId)/values/\(name)"
-
         var components = URLComponents(url: baseUrl.appendingPathComponent(path), resolvingAgainstBaseURL: true)
         components?.queryItems = [ URLQueryItem(name: "key", value: apiKey) ]
         guard let url = components?.url else { throw SheetsError("malformed url request") }
 
+        let key = url.absoluteString
+        if let cachedValues = cache[key] {
+            return cachedValues
+        }
+
         let request = URLRequest(url: url)
         let response = try makeRequest(request)
-        return response["values"]
+        let values = response["values"]
+        cache[key] = values
+
+        return values
     }
 
 }
