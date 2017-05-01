0c3892bcad1e3a6e9ab1170860b61fda311543ad
Ikhsan Assaat <ixnixnixn@yahoo.com>
2017-05-01 02:51:58 +0100

    Add logo and keys

--- Sources/poCore/Keys.swift
+++ Sources/poCore/Keys.swift
@@ -5,4 +5,10 @@ public struct Keys {
     public static let sheetsId = ProcessInfo.processInfo.environment["SHEETS_ID"] ?? ""
     public static let sheetOrder = ProcessInfo.processInfo.environment["SHEET_ORDER_NAME"] ?? ""
     public static let sheetCustomer = ProcessInfo.processInfo.environment["SHEET_CUSTOMER_NAME"] ?? ""
+
+    public struct Google {
+        public static let clientId = ProcessInfo.processInfo.environment["GOOGLE_CLIENT_ID"] ?? ""
+        public static let secret = ProcessInfo.processInfo.environment["GOOGLE_SECRET"] ?? ""
+    }
 }
+
